98eb1a45d6f1614337d2be6ddb28f1593c8dfdbf
Júlia Torres <juliadittoro@yahoo.com>
2021-09-10 13:22:19 -0300

    REST testado

--- src/main/java/com/juliatorres/cursomc/CursomcApplication.java
+++ src/main/java/com/juliatorres/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.juliatorres.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,5 @@ public class CursomcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
-
 }
+
