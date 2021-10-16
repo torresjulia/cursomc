574b320bd86b637533d16b62dcd67e274fadee76
Júlia Torres <juliadittoro@yahoo.com>
2021-10-16 16:26:45 -0300

    Produto e associacao muitos para muitos.

--- src/main/java/com/juliatorres/cursomc/domain/Categoria.java
+++ src/main/java/com/juliatorres/cursomc/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.juliatorres.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -20,6 +23,9 @@ public class Categoria implements Serializable {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id; 
 	private String nome; 
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria () {
 		
@@ -46,6 +52,13 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -63,4 +76,5 @@ public class Categoria implements Serializable {
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
+
 }
--- src/main/java/com/juliatorres/cursomc/resources/CategoriaResouce.java
+++ src/main/java/com/juliatorres/cursomc/resources/CategoriaResouce.java
@@ -29,5 +29,4 @@ public class CategoriaResouce {
 		return ResponseEntity.ok(service.findAll());
 	}
 	
-
 }
