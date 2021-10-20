40ff5fd776056ab57dc8c812b64ef57ca1786ac3
Júlia Torres <juliadittoro@yahoo.com>
2021-10-19 21:40:59 -0300

    Estado e Cidade

--- src/main/java/com/juliatorres/cursomc/CursomcApplication.java
+++ src/main/java/com/juliatorres/cursomc/CursomcApplication.java
@@ -2,14 +2,19 @@ package com.juliatorres.cursomc;
 
 import java.util.Arrays;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.juliatorres.cursomc.domain.Categoria;
+import com.juliatorres.cursomc.domain.Cidade;
+import com.juliatorres.cursomc.domain.Estado;
 import com.juliatorres.cursomc.domain.Produto;
 import com.juliatorres.cursomc.repositories.CategoriaRepository;
+import com.juliatorres.cursomc.repositories.CidadeRepository;
+import com.juliatorres.cursomc.repositories.EstadoRepository;
 import com.juliatorres.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -17,9 +22,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository; 
-	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -42,10 +50,26 @@ public class CursomcApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1); 
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2); 
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2)); 
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3)); 
+		
+
+		
+		
 		
 	}
 }
---
+++ src/main/java/com/juliatorres/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,13 @@
+package com.juliatorres.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.juliatorres.cursomc.domain.Cidade;
+
+@Repository 
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
+
+
