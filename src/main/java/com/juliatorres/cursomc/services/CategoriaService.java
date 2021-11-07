209ad98c4b039675314dd1948e58da653528aa94
Júlia Torres <juliadittoro@yahoo.com>
2021-11-07 14:25:17 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/juliatorres/cursomc/services/CategoriaService.java
+++ src/main/java/com/juliatorres/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.juliatorres.cursomc.domain.Categoria;
+import com.juliatorres.cursomc.dto.CategoriaDTO;
 import com.juliatorres.cursomc.repositories.CategoriaRepository;
 import com.juliatorres.cursomc.services.services.exceptions.DataIntegrityException;
 import com.juliatorres.cursomc.services.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
